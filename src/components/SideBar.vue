6f73cbe75014fc3e60d53aa5989e831aeefdd6ad
jihongyu <48473551+jhongyu@users.noreply.github.com>
2020-08-07 17:16:11 +0800

    committing initial code

---
+++ src/components/SideBar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+        <div class="header">
+            <button class="search">Search for places</button>
+        </div>
+        <div class="weather">
+            <div class="img"></div>
+            <div class="temperature">
+                {{ weatherData.consolidated_weather[0].the_temp }}
+            </div>
+            <div class="description">
+                {{ weatherData.consolidated_weather[0].weather_state_name }}
+            </div>
+        </div>
+        <div class="footer">
+            <div class="date">Today · {{ weatherData.time }}</div>
+            <div class="position">{{ weatherData.title }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cityId: '',
+      weatherData: ''
+    }
+  },
+  beforeMount() {
+    const that = this;
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0
+    };
+
+    function success(pos) {
+      const coord = pos.coords;
+      that.getCityId(coord.lantitude, coord.longitude);
+    }
+
+    function errer(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`)
+    }
+
+    navigator.geolocation.getCurrentPosition(success, errer, options);
+    fetch(`https://www.metaweather.com/api/location/${that.cityId}`).then(data => {
+      that.weatherData = data;
+    })
+  },
+  methods: {
+    getCityId(lantitude, longitude) {
+      fetch(`https://www.metaweather.com/api/location/search/?lattlong=${lantitude},${longitude}`).then(location => {
+        this.cityId = location.woeid;
+      })
+    }
+  }
+}
+</script>
