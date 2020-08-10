14fa6a57b8ec5b51e43eb824cdd1272b57d1a9de
jihongyu <48473551+jhongyu@users.noreply.github.com>
2020-08-10 20:43:36 +0800

    Add new component

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="card">
+    <div class="date">
+      <p>{{ todayWeather.applicable_date }}</p>
+    </div>
+    <div class="img">
+      <img src="" alt="" />
+    </div>
+    <div class="temperature">
+      <p class="max">{{ todayWeather.max_temp }}</p>
+      <p class="min">{{ todayWeather.min_temp }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'weatherCard',
+  props: ['todayWeather']
+}
+</script>
