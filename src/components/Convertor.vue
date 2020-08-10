20a62068094b6a93a10cc0a21b5c5db7a27e6675
jihongyu <48473551+jhongyu@users.noreply.github.com>
2020-08-10 21:58:41 +0800

    add convertor component

---
+++ src/components/Convertor.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="convert" :key="unit">{{ unit }}</button>
+</template>
+
+<script>
+export default {
+  name: 'convertor',
+  props: {
+    unit: String
+  },
+  methods: {
+    convert() {
+      this.$emit('unit-changed', this.unit);
+    }
+  }
+}
+</script>
